2a2169b17de54a5fe15c9242ea9eb7afc228466c
Sebastian Macke <sebastian@macke.de>
2013-01-06 13:24:56 +0100

    New system Object. Removed all variables from global scope, Clean up

--- js/system.js
+++ js/system.js
@@ -1,47 +1,113 @@
+// -------------------------------------------------
+// ------------------- SYSTEM ----------------------
+// -------------------------------------------------
 
-function CopyBinary(to, from, size, buffersrc, bufferdest) {
-    for (var i = 0; i < size; i++) bufferdest[to + i] = buffersrc[from + i];
+function System(termid, fbid) {
+    this.term = new Terminal(25, 80, termid);
+    DebugMessage("Terminal initialized");
+
+    this.ram = new RAM(0x2000000);
+    DebugMessage("RAM initialized");
+
+    this.cpu = new CPU(this.ram);
+    DebugMessage("CPU initialized");
+
+    this.uartdev = new UARTDev(this.term, this.cpu);
+    this.ethdev = new EthDev();
+    this.fbdev = new FBDev(fbid, this.ram);
+    DebugMessage("Devices initialized");
+
+    this.ram.AddDevice(this.uartdev, 0x90000000, 0x7);
+    this.ram.AddDevice(this.ethdev, 0x92000000, 0x1000);
+    this.ram.AddDevice(this.fbdev, 0x91000000, 0x1000);
+    DebugMessage("Devices added");
+
+    var terminput = new TerminalInput(this.uartdev);
+    DebugMessage("Terminal input initialized");
+
+    sys = this; // one global variable used by the abort() function
 }
 
-function ImageFinished(buffer) {
+System.prototype.PrintState = function() {
+    DebugMessage("Current state of the machine")
+    //DebugMessage("clock: " + hex8(cpu.clock));
+    DebugMessage("PC: " + hex8(this.cpu.pc));
+    DebugMessage("next PC: " + hex8(this.cpu.nextpc));
+    //DebugMessage("ins: " + hex8(cpu.ins));
+    //DebugMessage("main opcode: " + hex8(cpu.ins>>>26));
+    //DebugMessage("sf... opcode: " + hex8((cpu.ins>>>21)&0x1F));
+    //DebugMessage("op38. opcode: " + hex8((cpu.ins>>>0)&0x3CF));
+
+    for (var i = 0; i < 32; i += 4) {
+        DebugMessage("   r" + (i + 0) + ": " +
+            hex8(this.cpu.r[i + 0]) + "   r" + (i + 1) + ": " +
+            hex8(this.cpu.r[i + 1]) + "   r" + (i + 2) + ": " +
+            hex8(this.cpu.r[i + 2]) + "   r" + (i + 3) + ": " +
+            hex8(this.cpu.r[i + 3]));
+    }
+    
+    if (this.cpu.jumpdelayed) {
+        DebugMessage("delayed jump");
+    }    
+    if (this.cpu.delayedins) {
+        DebugMessage("delayed instruction");
+    }
+    if (this.cpu.SR_SM) {
+        DebugMessage("Supervisor mode");
+    }
+    else {
+        DebugMessage("User mode");
+    }
+    if (this.cpu.SR_TEE) {
+        DebugMessage("tick timer exception enabled");
+    }
+    if (this.cpu.SR_IEE) {
+        DebugMessage("interrupt exception enabled");
+    }
+    if (this.cpu.SR_DME) {
+        DebugMessage("data mmu enabled");
+    }
+    if (this.cpu.SR_IME) {
+        DebugMessage("instruction mmu enabled");
+    }
+    if (this.cpu.SR_LEE) {
+        DebugMessage("little endian enabled");
+    }
+    if (this.cpu.SR_CID) {
+        DebugMessage("context id enabled");
+    }
+    if (this.cpu.SR_F) {
+        DebugMessage("flag set");
+    }
+    if (this.cpu.SR_CY) {
+        DebugMessage("carry set");
+    }
+    if (this.cpu.SR_OV) {
+        DebugMessage("overflow set");
+    }
+}
+
+
+
+System.prototype.LoadImageAndStart = function(filename) {
+    DebugMessage("Loading Image");
+    var str = "Loading Image from Web Server (5 MB). Please wait ..."
+    for (var i = 0; i < str.length; i++) {
+        this.term.PutChar(str.charCodeAt(i));
+    }
+    LoadBinaryResource(filename, this.ImageFinished.bind(this));
+}
+
+System.prototype.ImageFinished = function(buffer) {
     var buffer8 = new Uint8Array(buffer);
     DebugMessage("Image loaded: " + buffer8.length + " bytes");
-    for (var i = 0; i < buffer8.length; i++) ram.uint8mem[i] = buffer8[i];
-    for (var i = 0; i < buffer8.length >>> 2; i++) ram.uint32mem[i] = Swap32(ram.uint32mem[i]); // big endian to little endian
-
+    for (var i = 0; i < buffer8.length; i++) this.ram.uint8mem[i] = buffer8[i];
+    for (var i = 0; i < buffer8.length >>> 2; i++) this.ram.uint32mem[i] = Swap32(this.ram.uint32mem[i]); // big endian to little endian
     DebugMessage("Starting emulation");
-    MainLoop();
+    this.MainLoop();
 }
 
-function MainLoop() {
-	cpu.Step(0x10000);
-    window.setTimeout(MainLoop, 0);
+System.prototype.MainLoop = function() {
+    this.cpu.Step(0x10000);
+    window.setTimeout(this.MainLoop.bind(this), 0);
 }
-
-var term = new Terminal(25, 80, "tty");
-DebugMessage("Terminal initialized");
-var ram = new RAM(0x2000000);
-DebugMessage("RAM initialized");
-var cpu = new CPU();
-DebugMessage("CPU initialized");
-
-var uartdev = new UARTDev(term);
-var ethdev = new EthDev();
-var fbdev = new FBDev("fb");
-DebugMessage("Devices initialized");
-
-ram.AddDevice(uartdev, 0x90000000, 0x7);
-ram.AddDevice(ethdev, 0x92000000, 0x1000);
-ram.AddDevice(fbdev, 0x91000000, 0x1000);
-DebugMessage("Devices added");
-
-var terminput = new TerminalInput(uartdev);
-DebugMessage("Terminal input initialized");
-
-DebugMessage("Loading Image");
-var str = "Loading Image from Web Server (5 MB). Please wait ..."
-for (var i = 0; i < str.length; i++) {
-    term.PutChar(str.charCodeAt(i));
-}
-
-LoadBinaryResource("bin/vmlinux.bin", ImageFinished);
