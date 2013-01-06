85136777377bdcd02b518469fdbf867e71a725c5
Sebastian Macke <sebastian@macke.de>
2013-01-06 15:01:34 +0100

    JSlint corrections

--- js/eth.js
+++ js/eth.js
@@ -4,37 +4,34 @@
 
 // Emulation of the basics of the dummy ethernet controller.
 
+
 function EthDev() {
-    this.ethreg0 = 0xa000;
-    this.ethreg38 = 0x22;	
-    this.ReadReg32 = function(r) {
-        switch(r)
-        {
-            case 0x0:
-                return this.ethreg0;
-                break;
-            case 0x38:
-                var ret = this.ethreg38;
-                if (this.ethreg38 == 0x1613) {
-                    this.ethreg38 = 0xffff;
-                }
-                if (this.ethreg38 == 0x22) {
-                    this.ethreg38 = 0x1613;
-                }
-                return ret;
-                break;
-            default:
-                return 0x0;
-                break;			
+    "use strict";
+    this.ethreg0 = 0xA000;
+    this.ethreg38 = 0x22;
+    this.ReadReg32 = function (r) {
+        switch (r) {
+        case 0x0:
+            return this.ethreg0;
+        case 0x38:
+            var ret = this.ethreg38;
+            if (this.ethreg38 === 0x1613) {
+                this.ethreg38 = 0xffff;
+            }
+            if (this.ethreg38 === 0x22) {
+                this.ethreg38 = 0x1613;
+            }
+            return ret;
+        default:
+            return 0x0;
         }
-    }
-	
-	this.WriteReg32 = function(r, x) {
-        switch(r)
-        {
-            case 0x0:
-                ethreg0 = x;
-                break;
-        }	
-	}
+    };
+
+    this.WriteReg32 = function (r, x) {
+        switch (r) {
+        case 0x0:
+            this.ethreg0 = x;
+            break;
+        }
+    };
 }
