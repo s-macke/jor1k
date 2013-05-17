ebd17ac807da0475a5c838a0c7d0936a8d218afd
s-macke <sebastian@macke.de>
2013-05-17 11:26:17 +0200

    Optional UintClampedArray and framerate chang to 10fps

--- js/framebuffer.js
+++ js/framebuffer.js
@@ -13,6 +13,8 @@ function FBDev(elemId, ram) {
     // create a new batch of pixels with the same
     // dimensions as the image:
     this.imageData = this.c.createImageData(this.width, this.height);
+    // address in ram where the pixeldata resides 
+    this.addr = 0x0;
 }
 
 FBDev.prototype.ReadReg32 = function (addr) {
@@ -30,22 +32,32 @@ FBDev.prototype.WriteReg32 = function (addr, value) {
 };
 
 FBDev.prototype.SetAddr = function (addr) {
-    if (typeof Uint8ClampedArray === 'undefined') return; // no framebuffer output available
-	this.buf8 = new Uint8ClampedArray(this.ram.mem, addr, this.imageData.data.length);
+    this.addr = addr;
+    if (typeof Uint8ClampedArray !== 'undefined')  {
+        this.buf8 = new Uint8ClampedArray(this.ram.mem, addr, this.imageData.data.length);
+    }
     this.Update();
 };
 
 FBDev.prototype.Update = function () {
-    if (!this.buf8) {
-        return;
-    }
-    // remove alpha channel. The buffer can then be used directly.
-    var i, n = this.width * this.height * 4;
-    for (i = 3; i < n; i += 4) {
-        this.buf8[i] = 0xFF;
+    var i=0, n = this.width * this.height * 4;
+    var data = this.imageData.data;
+    if (!this.buf8) { 
+        // copy the framebuffer byte by byte
+        var mem = this.ram.uint8mem;
+   	    for (i = 0; i < n; ++i) {
+            data[i] = mem[this.addr+i];
+        }
+    } else
+    {
+        // otherwise use set
+        data.set(this.buf8);
     }
 
-    this.imageData.data.set(this.buf8);
+    // remove alpha channel.
+  	for (i = 3; i < n; i += 4) {
+        data[i] = 0xFF;
+    }
     this.c.putImageData(this.imageData, 0, 0); // at coords 0,0
-    window.setTimeout(this.Update.bind(this), 500); // update every half a second
+    window.setTimeout(this.Update.bind(this), 100); // update 10 times a second. Maybe use requestAnimationFrame
 };
