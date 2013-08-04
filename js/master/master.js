c60293f8808cc5c6045fc190b66a5eaf65f0db7e
s-macke <sebastian@macke.de>
2013-08-04 15:25:54 -0700

    Correct framebuffer color order

--- js/master/master.js
+++ js/master/master.js
@@ -89,9 +89,9 @@ jor1kGUI.prototype.UpdateFramebuffer = function(buffer) {
     
     for (i = 0; i < n; ++i) {
         var x = buffer[i];
-        data[(i<<2)+0] = x&0xFF;
+        data[(i<<2)+0] = (x>>16)&0xFF;
         data[(i<<2)+1] = (x>>8)&0xFF;
-        data[(i<<2)+2] = (x>>16)&0xFF;
+        data[(i<<2)+2] = (x)&0xFF;
         data[(i<<2)+3] = 0xFF;
     }
 
