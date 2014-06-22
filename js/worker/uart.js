d6a766ace9d4fa59e55394b09d24dbe51ef48db8
Sebastian Macke <sebastian@macke.de>
2014-06-21 17:23:45 -0700

    Clean up uart code

--- js/worker/uart.js
+++ js/worker/uart.js
@@ -19,9 +19,10 @@ var UART_IIR_CTI = 0x0c; /* Character timeout */
 
 var UART_LCR_DLAB = 0x80; /* Divisor latch access bit */
 
+var UART_RXBUF = 0; /* R: Rx buffer, DLAB=0 */
+var UART_TXBUF = 0; /* W: Tx buffer, DLAB=0 */
 var UART_DLL = 0; /* R/W: Divisor Latch Low, DLAB=1 */
 var UART_DLH = 1; /* R/W: Divisor Latch High, DLAB=1 */
-
 var UART_IER = 1; /* R/W: Interrupt Enable Register */
 var UART_IIR = 2; /* R: Interrupt ID Register */
 var UART_FCR = 2; /* W: FIFO Control Register */
@@ -29,6 +30,7 @@ var UART_LCR = 3; /* R/W: Line Control Register */
 var UART_MCR = 4; /* W: Modem Control Register */
 var UART_LSR = 5; /* R: Line Status Register */
 var UART_MSR = 6; /* R: Modem Status Register */
+var UART_SCR = 7; /* R/W: Scratch Register*/
 
 
 // constructor
@@ -36,7 +38,6 @@ function UARTDev(outputdev, intdev) {
     this.intdev = intdev;
     this.odev = outputdev;
     this.Reset();  
-    this.fifo = new Array(); // receive fifo buffer. Simple JS push/shift O(N) implementation 
 }
 UARTDev.prototype.Reset = function() {
     this.LCR = 0x3; // Line Control, reset, character has 8 bits
@@ -49,12 +50,12 @@ UARTDev.prototype.Reset = function() {
     this.DLH = 0;
     this.FCR = 0x0; // FIFO Control;
     this.MCR = 0x0; // Modem Control
-    this.input = 0;
+    this.rxbuf = new Array(); // receive fifo buffer. Simple JS push/shift O(N) implementation 
 }
 
 // To prevent the character from being overwritten we use a javascript array-based fifo and immediately request a character timeout. 
 UARTDev.prototype.ReceiveChar = function(x) {
-    this.fifo.push(x);
+    this.rxbuf.push(x&0xFF);
     this.LSR |= UART_LSR_DATA_READY;
     this.ThrowCTI();
 
@@ -68,6 +69,7 @@ UARTDev.prototype.ThrowCTI = function() {
     if ((this.IIR != UART_IIR_RLSI) && (this.IIR != UART_IIR_RDI)) {
         this.IIR = UART_IIR_CTI;
         this.intdev.RaiseInterrupt(0x2);
+		DebugMessage("raise 0x2 with " + this.rxbuf);
     }
 };
 
@@ -97,7 +99,7 @@ UARTDev.prototype.NextInterrupt = function() {
 
 UARTDev.prototype.ClearInterrupt = function(line) {
     this.ints &= ~ (1 << line);
-    this.IIR = UART_IIR_NO_INT;
+    //this.IIR = UART_IIR_NO_INT;
     if (line != this.IIR) {
         return;
     }
@@ -105,7 +107,7 @@ UARTDev.prototype.ClearInterrupt = function(line) {
 };
 
 UARTDev.prototype.ReadReg8 = function(addr) {
-    if (this.LCR & UART_LCR_DLAB) {
+    if (this.LCR & UART_LCR_DLAB) {  // Divisor latch access bit
         switch (addr) {
         case UART_DLL:
             return this.DLL;
@@ -116,18 +118,17 @@ UARTDev.prototype.ReadReg8 = function(addr) {
         }
     }
     switch (addr) {
-    case 0:
+    case UART_RXBUF:
         {
-            var ret = 0x21;// !
-            this.input = 0;
+            var ret = 0x21; // !
             this.ClearInterrupt(UART_IIR_RDI);
             this.ClearInterrupt(UART_IIR_CTI);
-            var fifo_len = this.fifo.length;
-            if (fifo_len >= 1) {
-                ret = this.fifo.shift();
+            var rxbuf_len = this.rxbuf.length;
+            if (rxbuf_len >= 1) {
+                ret = this.rxbuf.shift();
             }
             // Due to shift(), the fifo buffer is now smaller. Perhaps we shifted the last byte?
-            if(fifo_len > 1) { // Still more bytes to read - immediately timeout
+            if(rxbuf_len > 1) { // Still more bytes to read - immediately timeout
                 this.LSR |= UART_LSR_DATA_READY;
                 this.ThrowCTI(); // Immediately timeout - we're ready to transfer
             }
@@ -182,11 +183,10 @@ UARTDev.prototype.WriteReg8 = function(addr, x) {
     }
 
     switch (addr) {
-    case 0:
+    case UART_TXBUF:
         this.LSR &= ~UART_LSR_FIFO_EMPTY;
-        this.odev.PutChar(x);
-        // Data is send with a latency of zero!
-        this.LSR |= UART_LSR_FIFO_EMPTY; // send buffer is empty					
+        this.odev.PutChar(x); // Data is send with a latency of zero!
+        this.LSR |= UART_LSR_FIFO_EMPTY; // txbuffer is empty					
         this.ThrowTHRI();
         break;
     case UART_IER:
