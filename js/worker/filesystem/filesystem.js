3e0b77dcce472a9ba655e660c115c9383434058e
Sebastian Macke <sebastian@macke.de>
2014-08-15 23:55:56 -0700

    filesystem: Change size adaptively if written

--- js/worker/filesystem/filesystem.js
+++ js/worker/filesystem/filesystem.js
@@ -357,8 +357,9 @@ FS.prototype.Rename = function(olddirid, oldname, newdirid, newname) {
 
 FS.prototype.Write = function(id, offset, count, GetByte) {
     var inode = this.inodes[id];
-    if (inode.size < (offset+count)) {
-        this.ChangeSize(id, offset+count);
+    if (inode.data.length < (offset+count)) {
+        this.ChangeSize(id, Math.floor(((offset+count)*3)/2) );
+        inode.size = offset+count;
     }
     for(var i=0; i<count; i++)
         inode.data[offset+i] = GetByte();
