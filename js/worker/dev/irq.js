ca080d69954b9d5d2984cd8652f01e249f447b00
Sebastian Macke <sebastian@macke.de>
2014-11-04 05:25:08 +0100

    IRQ: More debug messages

--- js/worker/dev/irq.js
+++ js/worker/dev/irq.js
@@ -17,7 +17,7 @@
 // +----------+-------------+----------+---------+
 // | 31       | 30          | 29 .. 16 | 15 .. 0 |
 // -----------+-------------+----------+---------+
-// | Reserved | IRQ Pending | SRC CORE | DATA |
+// | Reserved | IRQ Pending | SRC CORE | DATA    |
 // +----------+-------------+----------+---------+
 
 var OMPIC_IPI_CTRL_IRQ_ACK = (1 << 31);
@@ -79,6 +79,13 @@ IRQDev.prototype.WriteReg32 = function (addr, value) {
         if (flags & 1) { // irq gen
             if (dstcpu == cpuid) {
                 DebugMessage("Warning in IRQDev: Try to raise its own IRQ");
+            }
+            if (this.regs[(dstcpu<<1)+1] & OMPIC_IPI_STAT_IRQ_PENDING) {
+                DebugMessage("Warning in IRQDev: CPU " + cpuid + " raised irq on cpu " + dstcpu + " without previous acknowledge");
+                var h = new Int32Array(this.intdev.heap);
+                DebugMessage("The pc of cpu " + dstcpu + " is " + hex8(h[(dstcpu<<15) + 0x124 >> 2]));
+                DebugMessage("The IEE flag of cpu " + dstcpu + " is " + ( h[(dstcpu<<15) + 0x120 >> 2] & (1<<2)) );
+                DebugMessage("r9 of cpu " + dstcpu + " is " + hex8(h[(dstcpu<<15) + (0x9<<2) >> 2]));
             }
             this.regs[(dstcpu<<1)+1] = OMPIC_IPI_STAT_IRQ_PENDING | ((cpuid & 0x3fff) << 16) | irqno;
             this.intdev.RaiseSoftInterrupt(0x1, dstcpu);
